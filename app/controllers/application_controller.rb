0b0a13a05d821ca4feb0df1bf64f7ea00f8e2bab
Joshua Hume <valencelectron@hotmail.com>
2021-04-13 15:45:01 +0900

    study set create controller working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token, if: :devise_controller?, raise: false
 
+  def update_user_frontend(current_user)
+    render json: current_user.to_json(include: [study_sets:{ include: :words }])
+  end
+
 end
--- app/controllers/study_sets_controller.rb
+++ app/controllers/study_sets_controller.rb
@@ -1,5 +1,45 @@
 class StudySetsController < ApplicationController
     def index
-    
+
+    end
+
+    def create
+        study_set = StudySet.new(study_set_params)
+        study_set.user = current_user
+        if study_set.save
+            puts "Success!!!"
+            save_words(params[:words], study_set)
+        else
+            puts "OH NO"
+            study_set.save!
+        end
+    end
+    private
+
+    def study_set_params
+        params.require(:study_set).permit(:name, :public, :words)
+    end
+
+    def save_words(words, study_set)
+        failures = []
+        words.each do |word|
+            new_word = Word.new
+            new_word.study_set = study_set
+            new_word.kanji = word[:kanji]
+            new_word.yomikata = word[:yomikata]
+            new_word.definition = word[:definition]
+            if new_word.save
+                puts "Saved a new word successfully"
+            else
+                failures << new_word.errors
+                puts "NEW WORD FAILED TO SAVE"
+                new_word.save!
+            end
+        end
+        if failures.size.zero?
+            update_user_frontend(current_user)
+        else
+            render json: {errors: failures}.to_json
+        end
     end
 end
