32ed99fa1e9ed63efa2c726c6a828a7b83bc471f
Joshua Hume <valencelectron@hotmail.com>
2021-04-14 16:17:47 +0900

    updated study_set

--- app/models/study_set.rb
+++ app/models/study_set.rb
@@ -1,5 +1,5 @@
 class StudySet < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user, message: "That name has already been usedf.text_area :attribute" }
   belongs_to :user
   has_many :words, dependent: :destroy
 end
